059ee121cad50a96b055f776d155ca03c78357e5
ArtomMrdn <artommrdn47@gmail.com>
2022-10-21 12:18:55 +0300

    lesson 22 added

---
+++ 22-key-attribute/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    posts: [
+      { id: "abc1", name: "JS Basics" },
+      { id: "abc2", name: "JS Advanced" },
+      { id: "abc3", name: "React JS" },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.posts.map((post) => (
+          <h2 key={post.id}>{post.name}</h2>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
