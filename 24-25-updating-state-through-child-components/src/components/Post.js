8fd43f7472d022211ab779e8ba01344cbeff7f18
ArtomMrdn <artommrdn47@gmail.com>
2022-10-23 13:17:13 +0300

    lesson 24-25 added

---
+++ 24-25-updating-state-through-child-components/src/components/Post.js
@@ -0,0 +1,10 @@
+function Post(props) {
+  const { id, name, removePost } = props;
+  return (
+    <h2>
+      {name} <button onClick={() => removePost(id)}>delete</button>
+    </h2>
+  );
+}
+
+export default Post;
---
+++ 24-25-updating-state-through-child-components/src/components/Posts.js
@@ -0,0 +1,18 @@
+import Post from "./Post";
+
+function Posts(props) {
+  return (
+    <div>
+      {props.posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          name={post.name}
+          removePost={props.removePost}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
