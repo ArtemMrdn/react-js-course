e269fc48e9d0f5377e2760eb84a5d78205d4686e
ArtomMrdn <artommrdn47@gmail.com>
2022-11-16 12:58:02 +0200

    lesson 67 added

---
+++ 63-67-shop-project/src/components/Cart.js
@@ -0,0 +1,11 @@
+function Cart(props) {
+  const { quantity = 0 } = props;
+  return (
+    <div className='cart purple lighten-2 white-text'>
+      <i className='material-icons'>shopping_cart</i>
+      {quantity ? <span className='cart-quantity'>{quantity}</span> : null}
+    </div>
+  );
+}
+
+export default Cart;
--- 63-67-shop-project/src/components/Shop.js
+++ 63-67-shop-project/src/components/Shop.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import Preloader from "./Preloader";
 import GoodsList from "./GoodsList";
 import { API_KEY, API_URL } from "../config";
+import Cart from "./Cart";
 
 function Shop() {
   const [goods, setGoods] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
 
   useEffect(function getGoods() {
     fetch(API_URL, {
@@ -22,6 +24,7 @@ function Shop() {
 
   return (
     <main className='container content'>
+      <Cart quantity={order.length} />
       {loading ? <Preloader /> : <GoodsList goods={goods} />}
     </main>
   );
