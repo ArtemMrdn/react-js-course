6c1a7fe1a56f541d4fcb646b6030b98c31a8e16e
ArtomMrdn <artommrdn47@gmail.com>
2022-12-04 14:43:16 +0200

    lesson 72 added

--- 63-72-shop-project/src/components/Shop.js
+++ 63-72-shop-project/src/components/Shop.js
@@ -58,6 +58,36 @@ function Shop() {
       });
   }, []);
 
+  const incQuantity = (itemId) => {
+    const newOrder = order.map((el) => {
+      if (el.id === itemId) {
+        const newQuantity = el.quantity + 1;
+        return {
+          ...el,
+          quantity: newQuantity,
+        };
+      } else {
+        return el;
+      }
+    });
+    setOrder(newOrder);
+  };
+
+  const decQuantity = (itemId) => {
+    const newOrder = order.map((el) => {
+      if (el.id === itemId) {
+        const newQuantity = el.quantity - 1;
+        return {
+          ...el,
+          quantity: newQuantity >= 0 ? newQuantity : 0,
+        };
+      } else {
+        return el;
+      }
+    });
+    setOrder(newOrder);
+  };
+
   return (
     <main className='container content'>
       <Cart quantity={order.length} handleBasketShow={handleBasketShow} />
@@ -71,6 +101,8 @@ function Shop() {
           order={order}
           handleBasketShow={handleBasketShow}
           removeFromBasket={removeFromBasket}
+          incQuantity={incQuantity}
+          decQuantity={decQuantity}
         />
       )}
     </main>
