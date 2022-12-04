8bdedf320107041a47ad1c379e0c3a5dbaa1d29d
ArtomMrdn <artommrdn47@gmail.com>
2022-12-04 14:28:54 +0200

    lesson 71 added

--- 63-71-shop-project/src/components/Header.js
+++ 63-71-shop-project/src/components/Header.js
@@ -2,9 +2,7 @@ function Header() {
   return (
     <nav className='pink darken-2'>
       <div className='nav-wrapper'>
-        <a href='#' className='brand-logo'>
-          React Shop
-        </a>
+        <a className='brand-logo'>React Shop</a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
             <a href='sass.html'>Repo</a>
